2cb251d4e66c04a94f6e43eaeabfa1d104f1954a
Your Name <you@example.com>
2019-10-17 00:26:26 +0530

    Added team

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ var User = require("../models/user");
 router.get("/", function(req, res){
     res.render("./landing/index");
 });
+router.get("/team", function(req, res){
+    res.render("./landing/team");
+});
 
 module.exports = router;
